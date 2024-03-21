77571ba00e8d82c694d6aa6cb5e850dfcb13ac3c
Daniel Rodriguez <danielrodriguezalonso02@gmail.com>
2024-03-21 16:31:39 +0000

    Ultima actualizacion

--- main.c
+++ main.c
@@ -1,19 +1,121 @@
 #include <stdio.h>
-#include <stdlib.h> // Para usar malloc()
+#define MAX 50
 
 // Definición de la estructura
-struct Persona {
+struct Persona
+{
     int dni;
     char nombre[10];
     char apellidos[20];
     int numero_matricula;
+} *ptr;
+
+void inicializa_grupo()
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        ptr[i].dni = -1;
+    }
 };
 
-int main() {
+int matricular_alumno(int dni)
+{
+    // Comprobar si el ealumno ya esta matriculado
+    for (int i = 0; i < MAX; i++)
+    {
+        if (ptr[i].dni == dni)
+        {
+            return -2;
+        }
+    }
+
+    for (int i = 0; i < MAX; i++)
+    {
+        if (ptr[i].dni == -1)
+        {
+            ptr[i].dni = dni;
+            return i;
+        }
+    }
+}
+
+int desmatricular_alumno(int dni)
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        if (ptr[i].dni == dni)
+        {
+            ptr[i].dni = -1;
+            return i;
+        }
+    }
+    return -1;
+}
+
+int testea_alumnos(int posicion)
+{
+    if (posicion < 0 || posicion >= MAX)
+    {
+        return -2;
+    }
+    return ptr[posicion].dni;
+}
+
+int plazas_libres() {
+    int count;
+    count = 0;
+    for (int i = 0; i < MAX; i++)
+    {
+        if (ptr[i].dni == -1)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int plazas_ocupadas() {
+    int count;
+    count = 0;
+    for (int i = 0; i < MAX; i++)
+    {
+        if (ptr[i].dni != -1)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+void test_matricula(int dni) {
+    for (int i = 0; i < MAX; i++)
+    {
+        int dni1;
+        dni1 = dni + i;
+        int resultado = matricular_alumno(dni1);
+        if (resultado == -2)
+        {
+            printf("El alumno ya esta matriculado\n");
+        } else if (resultado == -2) {
+            printf("No hay plazas disponibles\n");
+        } else {
+            printf("El alumno con DNI %d ha sido matriculado en la posicion %d\n", dni1, resultado);
+        }
+    }
+}
+
+void test_desmatricula() {
+    
+}
+
+int main()
+{
     // Crear el vector de estructuras
-    struct Persona vector[50];
-    // Liberar la memoria asignada para el vector
-    free(vector);
-    // Actualizacion
+    struct Persona clase[MAX];
+    ptr = clase;
+    inicializa_grupo();
+    test_matricula(79072763);
+    desmatricular_alumno(79072763);
     return 0;
 }
